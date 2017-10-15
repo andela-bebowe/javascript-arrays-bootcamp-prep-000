878ae390d14c7be20b825a81ce059f31dad67bc4
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-15 15:18:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndOfArray() {
-  array.slice(0, array.length)
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
 }
